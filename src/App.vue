0b1a61a177f34db6f5c5187240b0833942308a82
Boris MAURENCE <12050570+llanas@users.noreply.github.com>
2024-01-23 23:45:08 +0100

    💄 Creating table by day

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
+import dayjs from 'dayjs';
 import { useReports } from '@/services/reports';
 
-const reports = ref<any[]>([]);
+const reportsByDay = ref<any[]>([]);
 
 onMounted(async () => {
-  console.log('mounted');
-  reports.value = await useReports().getReports();
+  const response = await useReports().getReports();
+  const mapByDay = new Map();
+  response.forEach((item) => {
+    const day = dayjs(item.timestamp).format('DD/MM/YYYY');
+    if (!mapByDay.has(day)) {
+      mapByDay.set(day, [item]);
+    }
+    mapByDay.get(day).push(item);
+  });
+  reportsByDay.value = Array.from(mapByDay.values());
 });
 </script>
 
@@ -15,17 +24,40 @@ onMounted(async () => {
   <table>
     <thead>
       <tr>
-        <th>date</th>
-        <th>surfline</th>
-        <th>allosurf</th>
-        <th>isSurfable</th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
       </tr>
     </thead>
     <tbody>
-      <tr v-for="item in reports.sort((a, b) => a.timestamp - b.timestamp)" :key="item.timestamp">
-        <td>{{ new Date(item.timestamp).toLocaleString() }}</td>
-        <td>{{ item.surfline.key }}</td>
-        <td>{{ item.allosurf?.s_wht || '' }}</td>
+      <tr v-for="day in reportsByDay" :key="day[0]">
+        <td
+          v-for="hours in day.sort((a: any, b: any) => a.timestamp - b.timestamp)"
+          :key="hours.timestamp"
+        >
+          {{ dayjs(hours.timestamp).format('HH:mm') }} - {{ hours.surfline.key }}
+        </td>
       </tr>
     </tbody>
   </table>
@@ -39,23 +71,5 @@ header {
 .logo {
   display: block;
   margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
 }
 </style>
